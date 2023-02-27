0a9f2075bf7249f9801f37281a612e07f1ae9f14
maomao <1714487678@qq.com>
2023-02-27 20:24:18 +0800

    feat: MNavLinks 组件支持 title

--- docs/nav/components/MNavLinks.vue
+++ docs/nav/components/MNavLinks.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { slugify } from '@mdit-vue/shared'
+
 import MNavLink from './MNavLink.vue'
 import type { NavLink } from './type'
 
-defineProps<{
+const props = defineProps<{
+  title: string
   items: NavLink[]
 }>()
+
+const formatTitle = computed(() => {
+  return slugify(props.title)
+})
 </script>
 
 <template>
+  <h2 v-if="title" :id="formatTitle" tabindex="-1">
+    {{ title }}
+    <a class="header-anchor" :href="`#${formatTitle}`" aria-hidden="true">#</a>
+  </h2>
   <div class="m-nav-links">
     <MNavLink
       v-for="{ icon, title, desc, link } in items"
