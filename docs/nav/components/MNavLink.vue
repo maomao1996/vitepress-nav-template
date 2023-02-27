da7dcab1618180271b05c02b9920396a18e460c5
maomao <1714487678@qq.com>
2023-02-27 20:16:06 +0800

    feat: 新增 MNavLink 和 MNavLinks 组件

---
+++ docs/nav/components/MNavLink.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import { NavLink } from './type'
+
+const props = defineProps<{
+  icon?: NavLink['icon']
+  title?: NavLink['title']
+  desc?: NavLink['desc']
+  link: NavLink['link']
+}>()
+
+const svg = computed(() => {
+  if (typeof props.icon === 'object') return props.icon.svg
+  return ''
+})
+</script>
+
+<template>
+  <a v-if="link" class="m-nav-link" :href="link" target="_blank" rel="noreferrer">
+    <article class="box">
+      <div class="box-header">
+        <div v-if="svg" class="icon" v-html="svg"></div>
+        <div v-else-if="icon && typeof icon === 'string'" class="icon">
+          <img :src="icon" :alt="title" onerror="this.parentElement.style.display='none'" />
+        </div>
+        <h6 v-if="title" class="title">{{ title }}</h6>
+      </div>
+      <p v-if="desc" class="desc">{{ desc }}</p>
+    </article>
+  </a>
+</template>
+
+<style lang="scss" scoped>
+.m-nav-link {
+  display: block;
+  border: 1px solid var(--vp-c-bg-soft);
+  border-radius: 8px;
+  height: 100%;
+  cursor: pointer;
+  transition: all 0.3s;
+  &:hover {
+    background-color: var(--vp-c-bg-soft);
+  }
+
+  .box {
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+    height: 100%;
+    color: var(--vp-c-text-1);
+    &-header {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  .icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 12px;
+    border-radius: 6px;
+    width: 48px;
+    height: 48px;
+    font-size: 24px;
+    background-color: var(--vp-c-mute);
+    transition: background-color 0.25s;
+    :deep(svg) {
+      width: 24px;
+      fill: currentColor;
+    }
+    :deep(img) {
+      border-radius: 4px;
+      width: 24px;
+    }
+  }
+
+  .title {
+    overflow: hidden;
+    flex-grow: 1;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    line-height: 48px;
+    font-size: 16px;
+    font-weight: 600;
+  }
+
+  .desc {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    flex-grow: 1;
+    margin: 10px 0 0;
+    line-height: 20px;
+    font-size: 12px;
+    color: var(--vp-c-text-2);
+  }
+}
+
+@media (max-width: 960px) {
+  .m-nav-link {
+    .box {
+      padding: 8px;
+    }
+    .icon {
+      width: 40px;
+      height: 40px;
+    }
+    .title {
+      line-height: 40px;
+      font-size: 14px;
+    }
+  }
+}
+</style>
---
+++ docs/nav/components/MNavLinks.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import MNavLink from './MNavLink.vue'
+import type { NavLink } from './type'
+
+defineProps<{
+  items: NavLink[]
+}>()
+</script>
+
+<template>
+  <div class="m-nav-links">
+    <MNavLink
+      v-for="{ icon, title, desc, link } in items"
+      :key="link"
+      :icon="icon"
+      :title="title"
+      :desc="desc"
+      :link="link"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.m-nav-links {
+  --gap: 10px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
+  grid-row-gap: var(--gap);
+  grid-column-gap: var(--gap);
+  grid-auto-flow: row dense;
+  justify-content: center;
+  margin-top: var(--gap);
+}
+
+@each $media, $size in (500px: 140px, 640px: 155px, 768px: 175px, 960px: 200px, 1440px: 240px) {
+  @media (min-width: $media) {
+    .m-nav-links {
+      grid-template-columns: repeat(auto-fill, minmax($size, 1fr));
+    }
+  }
+}
+
+@media (min-width: 960px) {
+  .m-nav-links {
+    --gap: 20px;
+  }
+}
+</style>
