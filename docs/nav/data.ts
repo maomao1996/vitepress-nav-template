e6d573cc9b3b81eb40c8d0111a1bdb756db28c09
maomao <1714487678@qq.com>
2023-03-28 22:54:40 +0800

    update: 更新导航站点数据

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -22,6 +22,12 @@ export const NAV_DATA: NavData[] = [
         link: 'https://tinypng.com'
       },
       {
+        icon: 'https://devtool.tech/logo.svg',
+        title: '开发者武器库',
+        desc: '开发者武器库，做开发者最专业最好用的专业工具箱',
+        link: 'https://devtool.tech'
+      },
+      {
         icon: 'https://tool.lu/favicon.ico',
         title: '在线工具',
         desc: '开发人员的工具箱',
@@ -35,7 +41,60 @@ export const NAV_DATA: NavData[] = [
       }
     ]
   },
-
+  {
+    title: 'AI 导航',
+    items: [
+      {
+        icon: '/icons/chatgpt.png',
+        title: 'ChatGPT（最强）',
+        link: 'https://chat.openai.com/chat'
+      },
+      {
+        icon: 'https://www.notion.so/images/logo-ios.png',
+        title: 'Notion AI（笔记）',
+        link: 'https://www.notion.so'
+      },
+      {
+        icon: 'https://www.midjourney.com/apple-touch-icon.png',
+        title: 'Midjourney（绘画）',
+        link: 'https://www.midjourney.com'
+      },
+      {
+        icon: 'https://global-uploads.webflow.com/59deb588800ae30001ec19c9/5d4891e0e260e3c1bc37b100_beautiful%20ai%20favicon%20%20blue%20square.png',
+        title: 'Beautiful.ai（PPT）',
+        link: 'https://www.beautiful.ai'
+      }
+    ]
+  },
+  {
+    title: '茂茂的站点导航',
+    items: [
+      {
+        icon: '/logo.png',
+        title: '前端日常笔记',
+        desc: '日常笔记记录（零零散散啥都记系列）',
+        link: 'https://github.com/maomao1996/daily-notes'
+      },
+      {
+        icon: '/logo.png',
+        title: '前端思维导图',
+        desc: '用思维导图的方式总结个人所学知识',
+        link: 'https://mindmap.fe-mm.com'
+      },
+      {
+        icon: 'https://qwerty.fe-mm.com/apple-touch-icon.png',
+        title: 'Qwerty Learner',
+        desc: '为键盘工作者设计的单词记忆与英语肌肉记忆锻炼软件',
+        link: 'https://qwerty.fe-mm.com'
+      },
+      {
+        icon: '/logo.png',
+        title: 'mmPlayer',
+        desc: 'mmPlayer 在线音乐播放器',
+        link: 'https://netease-music.fe-mm.com'
+      }
+    ]
+  },
   {
     title: 'React 生态',
     items: [
@@ -226,10 +285,39 @@ export const NAV_DATA: NavData[] = [
     ]
   },
   {
+    title: '小程序相关',
+    items: [
+      {
+        icon: 'https://res.wx.qq.com/a/wx_fed/assets/res/OTE0YTAw.png',
+        title: '微信小程序文档',
+        desc: '微信小程序官方开发者文档',
+        link: 'https://developers.weixin.qq.com/miniprogram/dev/framework/'
+      },
+      {
+        icon: '/icons/taro.svg',
+        title: 'Taro',
+        desc: '多端统一开发解决方案',
+        link: 'https://taro.jd.com'
+      },
+      {
+        icon: 'https://web-assets.dcloud.net.cn/unidoc/zh/icon.png',
+        title: 'uni-app',
+        desc: '一个使用 Vue.js 开发所有前端应用的框架',
+        link: 'https://uniapp.dcloud.net.cn'
+      },
+      {
+        icon: 'https://mpxjs.cn/favicon.ico',
+        title: 'Mpx',
+        desc: '增强型跨端小程序框架',
+        link: 'https://mpxjs.cn'
+      }
+    ]
+  },
+  {
     title: 'Node 相关',
     items: [
       {
-        icon: 'https://nodejs.org/static/images/favicons/apple-touch-icon.png',
+        icon: '/icons/nodejs.svg',
         title: 'Node.js',
         desc: 'Node.js 是一个基于 Chrome V8 引擎的 JavaScript 运行环境',
         link: 'https://nodejs.org/zh-cn'
@@ -241,7 +329,7 @@ export const NAV_DATA: NavData[] = [
         link: 'https://expressjs.com'
       },
       {
-        icon: 'https://nodejs.org/static/images/favicons/',
+        icon: '/icons/koa.svg',
         title: 'Koa',
         desc: '基于 Node.js 平台的下一代 web 开发框架',
         link: 'https://koajs.com'
@@ -257,6 +345,42 @@ export const NAV_DATA: NavData[] = [
         title: 'Nest.js 中文文档',
         desc: '用于构建高效且可伸缩的服务端应用程序的渐进式 Node.js 框架',
         link: 'https://docs.nestjs.cn'
+      }
+    ]
+  },
+  {
+    title: '可视化',
+    items: [
+      {
+        icon: 'https://echarts.apache.org/zh/images/favicon.png',
+        title: 'ECharts',
+        desc: '一个基于 JavaScript 的开源可视化图表库',
+        link: 'https://echarts.apache.org/zh/index.html'
+      },
+      {
+        icon: 'https://antv.vision/icons/icon-72x72.png',
+        title: 'AntV',
+        desc: '蚂蚁集团全新一代数据可视化解决方案，致力于提供一套简单方便、专业可靠、无限可能的数据可视化最佳实践。',
+        link: 'https://antv.vision/zh/'
+      },
+      {
+        icon: 'https://d3js.org/favicon.png',
+        title: 'D3.js',
+        desc: '一个遵循 Web 标准用于可视化数据的 JavaScript 库',
+        link: 'https://d3js.org'
+      },
+      {
+        icon: 'https://www.chartjs.org/favicon.ico',
+        title: 'Chart.js',
+        desc: '一个简单而灵活的 JavaScript 图表库',
+        link: 'https://www.chartjs.org'
+      },
+      {
+        icon: 'https://threejs.org/files/favicon.ico',
+        // icon: 'https://threejs.org/files/favicon_white.ico',
+        title: 'Three.js',
+        desc: 'JavaScript 3d 库',
+        link: 'https://threejs.org'
       }
     ]
   },
@@ -308,6 +432,64 @@ export const NAV_DATA: NavData[] = [
     ]
   },
   {
+    title: '站点生成器',
+    items: [
+      {
+        icon: 'https://astro.build/favicon.svg',
+        title: 'Astro',
+        desc: '一个现代化的轻量级静态站点生成器',
+        link: 'https://astro.build'
+      },
+      {
+        icon: 'https://cn.vuejs.org/logo.svg',
+        title: 'VitePress',
+        desc: '由 Vite 和 Vue 驱动的静态网站生成器',
+        link: 'https://vitepress.dev'
+      },
+      {
+        icon: 'https://cn.vuejs.org/logo.svg',
+        title: 'VuePress',
+        desc: 'Vue 驱动的静态网站生成器',
+        link: 'https://vuepress.vuejs.org/zh'
+      },
+      {
+        icon: 'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
+        title: 'dumi',
+        desc: '基于 Umi 为组件研发而生的静态站点框架',
+        link: 'https://d.umijs.org'
+      },
+      {
+        icon: 'https://docusaurus.io/zh-CN/img/docusaurus.ico',
+        title: 'Docusaurus',
+        desc: '基于 React 的静态网站生成器',
+        link: 'https://docusaurus.io/zh-CN'
+      }
+    ]
+  },
+  {
+    title: '图标库',
+    items: [
+      {
+        icon: 'https://img.alicdn.com/imgextra/i4/O1CN01Z5paLz1O0zuCC7osS_!!6000000001644-55-tps-83-82.svg',
+        title: 'iconfont',
+        desc: '国内功能很强大且图标内容很丰富的矢量图标库，提供矢量图标下载、在线存储、格式转换等功能',
+        link: 'https://www.iconfont.cn'
+      },
+      {
+        icon: 'https://lf1-cdn2-tos.bytegoofy.com/bydesign/iconparksite/logo.svg',
+        title: 'IconPark 图标库',
+        desc: 'IconPark图标库是一个通过技术驱动矢量图标样式的开源图标库，可以实现根据单一 SVG 源文件变换出多种主题， 具备丰富的分类、更轻量的代码和更灵活的使用场景；致力于构建高质量、统一化、可定义的图标资源，让大多数人都能够选择适合自己的风格图标',
+        link: 'https://iconpark.oceanengine.com/official'
+      },
+      {
+        icon: 'https://emoji.muan.co/appicon.png',
+        title: 'Emoji searcher',
+        desc: 'Emoji 表情大全',
+        link: ''
+      }
+    ]
+  },
+  {
     title: '前端学习资料',
     items: [
       {
@@ -321,6 +503,12 @@ export const NAV_DATA: NavData[] = [
         title: '菜鸟教程',
         desc: '学的不仅是技术，更是梦想！',
         link: 'https://www.runoob.com'
+      },
+      {
+        icon: '/icons/es6.svg',
+        title: 'ES6 入门教程',
+        desc: '阮一峰的网络日志',
+        link: 'http://es6.ruanyifeng.com'
       }
     ]
   },
@@ -375,25 +563,43 @@ export const NAV_DATA: NavData[] = [
     ]
   },
   {
-    title: '摸摸鱼',
+    title: '摸鱼专用',
     items: [
-      {
-        icon: 'https://www.youtube.com/s/desktop/014dbbed/img/favicon_48x48.png',
-        title: 'YouTube',
-        desc: '',
-        link: 'https://www.youtube.com'
-      },
-      {
-        icon: '/icons/twitter.svg',
-        title: 'Twitter',
-        desc: '',
-        link: 'https://twitter.com'
-      },
       {
         icon: 'https://momoyu.cc/icon-192.png',
         title: '摸摸鱼热榜',
         // desc: '聚合每日热门、搞笑、有趣、适合摸鱼的资讯',
         link: 'https://momoyu.cc'
+      },
+      {
+        icon: 'https://v.qq.com/favicon.ico',
+        title: '腾讯视频',
+        // desc: '中国领先的在线视频媒体平台，海量高清视频在线观看',
+        link: 'https://v.qq.com'
+      },
+      {
+        icon: 'https://static.hdslb.com/mobile/img/512.png',
+        title: '哔哩哔哩',
+        // desc: '',
+        link: 'https://www.bilibili.com'
+      },
+      {
+        icon: 'https://www.youtube.com/s/desktop/014dbbed/img/favicon_48x48.png',
+        title: 'YouTube',
+        // desc: '',
+        link: 'https://www.youtube.com'
+      },
+      {
+        icon: '/icons/twitter.svg',
+        title: 'Twitter',
+        // desc: '',
+        link: 'https://twitter.com'
+      },
+      {
+        icon: '/icons/pixiv.png',
+        title: 'Pixiv',
+        // desc: '',
+        link: 'https://www.pixiv.net'
       }
     ]
   }
