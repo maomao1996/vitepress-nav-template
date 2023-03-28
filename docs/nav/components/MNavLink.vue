6ead13d782f68a4813a879141bb3875f75183758
maomao <1714487678@qq.com>
2023-03-28 22:52:42 +0800

    feat: 优化导航组件

--- docs/nav/components/MNavLink.vue
+++ docs/nav/components/MNavLink.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue'
+import { withBase } from 'vitepress'
 import { slugify } from '@mdit-vue/shared'
 
 import { NavLink } from './type'
@@ -30,7 +31,11 @@ const svg = computed(() => {
       <div class="box-header">
         <div v-if="svg" class="icon" v-html="svg"></div>
         <div v-else-if="icon && typeof icon === 'string'" class="icon">
-          <img :src="icon" :alt="title" onerror="this.parentElement.style.display='none'" />
+          <img
+            :src="withBase(icon)"
+            :alt="title"
+            onerror="this.parentElement.style.display='none'"
+          />
         </div>
         <h5 v-if="title" :id="formatTitle" class="title">{{ title }}</h5>
       </div>
@@ -41,20 +46,27 @@ const svg = computed(() => {
 
 <style lang="scss" scoped>
 .m-nav-link {
+  --m-nav-icon-box-size: 40px;
+  --m-nav-icon-size: 24px;
+  --m-nav-box-gap: 12px;
+
   display: block;
   border: 1px solid var(--vp-c-bg-soft);
   border-radius: 8px;
   height: 100%;
-  cursor: pointer;
-  transition: all 0.3s;
+  background-color: var(--vp-c-bg-soft);
+  transition: all 0.25s;
   &:hover {
-    background-color: var(--vp-c-bg-soft);
+    box-shadow: var(--vp-shadow-2);
+    border-color: var(--vp-c-brand);
+    text-decoration: initial;
+    background-color: var(--vp-c-bg-soft-up);
   }
 
   .box {
     display: flex;
     flex-direction: column;
-    padding: 16px;
+    padding: var(--m-nav-box-gap);
     height: 100%;
     color: var(--vp-c-text-1);
     &-header {
@@ -67,20 +79,20 @@ const svg = computed(() => {
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-right: 12px;
+    margin-right: calc(var(--m-nav-box-gap) - 2px);
     border-radius: 6px;
-    width: 48px;
-    height: 48px;
-    font-size: 24px;
-    background-color: var(--vp-c-mute);
+    width: var(--m-nav-icon-box-size);
+    height: var(--m-nav-icon-box-size);
+    font-size: var(--m-nav-icon-size);
+    background-color: var(--vp-c-bg-soft-down);
     transition: background-color 0.25s;
     :deep(svg) {
-      width: 24px;
+      width: var(--m-nav-icon-size);
       fill: currentColor;
     }
     :deep(img) {
       border-radius: 4px;
-      width: 24px;
+      width: var(--m-nav-icon-size);
     }
   }
 
@@ -89,7 +101,7 @@ const svg = computed(() => {
     flex-grow: 1;
     white-space: nowrap;
     text-overflow: ellipsis;
-    line-height: 48px;
+    line-height: var(--m-nav-icon-box-size);
     font-size: 16px;
     font-weight: 600;
   }
@@ -101,8 +113,8 @@ const svg = computed(() => {
     overflow: hidden;
     text-overflow: ellipsis;
     flex-grow: 1;
-    margin: 10px 0 0;
-    line-height: 20px;
+    margin: calc(var(--m-nav-box-gap) - 2px) 0 0;
+    line-height: 1.5;
     font-size: 12px;
     color: var(--vp-c-text-2);
   }
@@ -110,15 +122,11 @@ const svg = computed(() => {
 
 @media (max-width: 960px) {
   .m-nav-link {
-    .box {
-      padding: 8px;
-    }
-    .icon {
-      width: 40px;
-      height: 40px;
-    }
+    --m-nav-icon-box-size: 36px;
+    --m-nav-icon-size: 20px;
+    --m-nav-box-gap: 8px;
+
     .title {
-      line-height: 40px;
       font-size: 14px;
     }
   }
--- docs/nav/components/MNavLinks.vue
+++ docs/nav/components/MNavLinks.vue
@@ -18,7 +18,7 @@ const formatTitle = computed(() => {
 <template>
   <h2 v-if="title" :id="formatTitle" tabindex="-1">
     {{ title }}
-    <a class="header-anchor" :href="`#${formatTitle}`" aria-hidden="true">#</a>
+    <a class="header-anchor" :href="`#${formatTitle}`" aria-hidden="true"></a>
   </h2>
   <div class="m-nav-links">
     <MNavLink
@@ -34,14 +34,14 @@ const formatTitle = computed(() => {
 
 <style lang="scss" scoped>
 .m-nav-links {
-  --gap: 10px;
+  --m-nav-gap: 10px;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
-  grid-row-gap: var(--gap);
-  grid-column-gap: var(--gap);
+  grid-row-gap: var(--m-nav-gap);
+  grid-column-gap: var(--m-nav-gap);
   grid-auto-flow: row dense;
   justify-content: center;
-  margin-top: var(--gap);
+  margin-top: var(--m-nav-gap);
 }
 
 @each $media, $size in (500px: 140px, 640px: 155px, 768px: 175px, 960px: 200px, 1440px: 240px) {
@@ -54,7 +54,7 @@ const formatTitle = computed(() => {
 
 @media (min-width: 960px) {
   .m-nav-links {
-    --gap: 20px;
+    --m-nav-gap: 20px;
   }
 }
 </style>
