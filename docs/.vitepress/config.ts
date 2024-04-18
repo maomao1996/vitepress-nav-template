6f92f9d4f42228cb2aecd4ee431dd69f55583c64
maomao <1714487678@qq.com>
2024-04-18 18:18:43 +0800

    build(vitepress): 优化主题配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -31,10 +31,11 @@ export default defineConfig({
 
     nav,
     sidebar,
+
     /* 右侧大纲配置 */
     outline: {
       level: 'deep',
-      label: '本页目录',
+      label: '目录',
     },
 
     socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996/vitepress-nav-template' }],
@@ -44,15 +45,26 @@ export default defineConfig({
       copyright: 'Copyright © 2019-present maomao',
     },
 
-    darkModeSwitchLabel: '外观',
-    returnToTopLabel: '返回顶部',
-    lastUpdatedText: '上次更新',
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      },
+    },
 
     docFooter: {
       prev: '上一篇',
       next: '下一篇',
     },
 
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+
+    /*** 自定义配置 ***/
     visitor: {
       badgeId: 'maomao1996.vitepress-nav-template',
     },
